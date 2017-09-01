bb85179e3b56314b15a95a7191a87a0f2ad8847c
Anthony <30807672+Caccavella@users.noreply.github.com>
2017-09-01 14:08:03 -0600

    Add files via upload

---
+++ routers/wizardrouter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+// import Wizard from './Wizard/Wizard/Wizard';
+import StepOne from './Wizard/StepOne/StepOne';
+import StepTwo from './Wizard/StepTwo/StepTwo';
+import StepThree from './Wizard/StepThree/StepThree';
+import StepFour from './Wizard/StepFour/StepFour';
+import StepFive from './Wizard/StepFive/StepFive';
+
+export default (
+    <Router>
+        <div>
+            <Route exact path='/' component={Wizard} />
+            <Route path='/StepOne' component={StepOne} />
+            <Route path='/StepTwo' component={StepTwo} />
+            <Route path='/StepThree' component={StepThree} />
+            <Route path='/StepFour' component={StepFour} />
+            <Route path='/StepFive' component={StepFive} />
+        </div>
+    </Router>    
+)
+// `/api/${user.id}`
